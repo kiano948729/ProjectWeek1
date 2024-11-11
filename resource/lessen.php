a7205e3950fc1bcd59cac8acc056f84af5445e5c
crowwraith <1185615@student.roc-nijmegen.nl>
2024-11-11 12:37:25 +0100

    Update lessen.php
    test test

---
+++ resource/lessen.php
@@ -0,0 +1,30 @@
+<h1>
+    de lessen in deze opleiding
+</h1>
+<div>
+    website 1
+</div>
+<div>
+    basis-programmeren
+</div>
+<div>
+    engels 
+</div>
+<div>
+    nederlands  
+</div>
+<div>
+    rekenen
+</div>
+<div>
+    burgerschap
+</div>
+<div>
+    SLB
+</div>
+<div>
+    sport op maat
+</div>
+<div>
+    projectweek
+</div>

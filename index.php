991821958bbb257c6a08e2240e2b53f03aca4653
kiano948729 <148862757+kiano948729@users.noreply.github.com>
2024-11-11 09:35:41 +0100

    project aangemaakt
    alle hoofd attributen zijn aangemaakt

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://kit.fontawesome.com/5321476408.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+
+    </main>
+    <footer>
+        
+    </footer>
+    
+</body>
+</html>

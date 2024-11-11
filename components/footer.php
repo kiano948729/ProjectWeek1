d81f23a783678f91485bf60ba5352b890d0f1084
Joe74012 <joewillems@upcmail.nl>
2024-11-11 13:51:25 +0100

    w

--- components/footer.php
+++ components/footer.php
@@ -1,18 +1,40 @@
 <link rel="stylesheet" href="css/style.css">
-<div class="ButtonsMedia">
-    <div class="ButtonsMedia2">
-        <a href="https://www.youtube.com/@rocnijmegen/videos">
-            <i class="fa-brands fa-square-youtube"></i>
-        </a>
-        <a href="https://www.facebook.com/rocnijmegen/?locale=nl_NL">
-            <i class="fa-brands fa-square-facebook"></i>
-        </a>
-        <a href="https://www.instagram.com/rocnijmegen/">
-            <i class="fa-brands fa-square-instagram"></i>
-        </a>
-        <a href="https://x.com/rocnijmegen">
-            <i class="fa-brands fa-square-x-twitter"></i>
-        </a>
-</div>
 
-</div>
+<footer class="Bottom-row">
+    <div class="invisible-left-row-bottom"></div>
+        <nav class="middle-row-bottom">
+            <div class="middle-row-texts">
+                <a href="About.html" class="White-Text">About</a>
+            </div>
+            <div class="middle-row-texts">
+                <h3 class="White-Text">Jobs</h3>
+            </div>
+            <div class="middle-row-texts">
+                <h3 class="yellow-Text">Legal</h3>
+            </div>
+        </nav>
+        <div class="invisible-right-row-bottom"></div>
+        <div class="right-row-bottom">
+            <div class="icons-bottom-row">
+                <a href="https://www.youtube.com/@rocnijmegen/videos">
+                    <i class="fa-brands fa-square-youtube"></i>
+                </a>
+            </div>
+            <div class="icons-bottom-row">
+                <a href="https://www.facebook.com/rocnijmegen/?locale=nl_NL">
+                    <i class="fa-brands fa-square-facebook"></i>
+                </a>
+            </div>
+            <div class="icons-bottom-row">
+                <a href="https://www.instagram.com/rocnijmegen/">
+                    <i class="fa-brands fa-square-instagram"></i>
+                </a>
+            </div>
+            <div class="icons-bottom-row">
+                <a href="https://x.com/rocnijmegen">
+                    <i class="fa-brands fa-square-x-twitter"></i>
+                </a>
+            </div>
+        </div>
+        <!--einde bodem rij--> 
+</footer>

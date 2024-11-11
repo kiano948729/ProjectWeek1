ceb3c46c9ab39d45851f28f3dcb3384cb82ca863
kiano948729 <148862757+kiano948729@users.noreply.github.com>
2024-11-11 09:44:56 +0100

    test
    tes

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,16 +8,18 @@
     <script src="https://kit.fontawesome.com/5321476408.js" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
+
 <body>
     <header>
-
+        <?php require_once("components/header.php"); ?>
     </header>
     <main>
 
     </main>
     <footer>
-        
+        <?php require_once("components/footer.php"); ?>
     </footer>
-    
+
 </body>
+
 </html>

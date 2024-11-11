464cedb39080c0a8c3f0471d329bbd34a2c1c019
Joe74012 <joewillems@upcmail.nl>
2024-11-11 13:16:53 +0100

    w

--- resource/overig.php
+++ resource/overig.php
@@ -1,7 +1,7 @@
 <div>
     <p>
-        <h1>3 bedrijven</h1>
-        <h2>de bedrijven die java gebruiken: Portbase, Picnic en Axiom Software Solutions</h2>
+        <h1 class="TextZwart">3 bedrijven</h1>
+        <h2 class="TextZwart">de bedrijven die java gebruiken: Portbase, Picnic en Axiom Software Solutions</h2>
     </p>
 </div>
 

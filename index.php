7460285be0f6d8dd2d99dcad792a40357f0f59a4
Joe74012 <joewillems@upcmail.nl>
2024-11-11 11:20:04 +0100

    added code

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <script src="https://kit.fontawesome.com/5321476408.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/61f819b69c.js" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>

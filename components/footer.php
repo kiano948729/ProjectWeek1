db31c4ae388633ec8a7078ac05ea22cef2ddc8fc
Joe74012 <joewillems@upcmail.nl>
2024-11-11 13:38:54 +0100

    w

--- components/footer.php
+++ components/footer.php
@@ -1,15 +1,18 @@
 <link rel="stylesheet" href="css/style.css">
 <div class="ButtonsMedia">
-<a href="https://www.youtube.com/@rocnijmegen/videos">
-    <i class="fa-brands fa-square-youtube"></i>
-</a>
-<a href="https://www.facebook.com/rocnijmegen/?locale=nl_NL">
-    <i class="fa-brands fa-square-facebook"></i>
-</a>
-<a href="https://www.instagram.com/rocnijmegen/">
-    <i class="fa-brands fa-square-instagram"></i>
-</a>
-<a href="https://x.com/rocnijmegen">
-    <i class="fa-brands fa-square-x-twitter"></i>
-</a>
+    <div class="ButtonsMedia2">
+        <a href="https://www.youtube.com/@rocnijmegen/videos">
+            <i class="fa-brands fa-square-youtube"></i>
+        </a>
+        <a href="https://www.facebook.com/rocnijmegen/?locale=nl_NL">
+            <i class="fa-brands fa-square-facebook"></i>
+        </a>
+        <a href="https://www.instagram.com/rocnijmegen/">
+            <i class="fa-brands fa-square-instagram"></i>
+        </a>
+        <a href="https://x.com/rocnijmegen">
+            <i class="fa-brands fa-square-x-twitter"></i>
+        </a>
+</div>
+
 </div>

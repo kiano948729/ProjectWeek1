2f42963f93688838b106dc4ad540768604fb3f8b
Joe74012 <joewillems@upcmail.nl>
2024-11-13 08:54:28 +0100

    w

---
+++ resource/lessen.php
@@ -0,0 +1,34 @@
+<h1>
+    de lessen in deze opleiding
+</h1>
+<div class="lessen-Website">
+   <P>website 1</P>
+</div>
+<div class="lessen-Website">
+    <p>basis-programmeren</p>
+</div>
+<div class="lessen-Website">
+    <p>engels</p> 
+</div>
+<div class="lessen-Website">
+    <p>nederlands</p>
+</div>
+<div class="lessen-Website">
+    <p>rekenen</p>
+</div>
+<div class="lessen-Website">
+    <p>burgerschap</p>
+</div>
+<div class="lessen-Website">
+    <p>SLB</p>
+</div>
+<div class="lessen-Website">
+   <p>sport op maat</p>
+</div>
+<div class="lessen-Website">
+    <p>projectweek</p>
+</div>
+<?php
+$current_page = basename($_SERVER['PHP_SELF']);
+?>
+
---
+++ resource/overig.php
@@ -0,0 +1,15 @@
+<div class="ContainerBedrijven">
+    <h1 class="TextZwart">3 bedrijven</h1>
+    <p>    
+        <h2 class="TextZwart">Er zijn verschillende bedrijven die java gebruiken, zoals Portbase, Picnic en Axiom Software Solutions</h2>
+    </p>
+</div>
+<div class="ContainerVakanties">
+    <h1 class="TextZwart">Vakanties</h1>
+    <p>    
+        <h2 class="TextZwart">De vakantie tijden zijn: December: 23 dec '24 - 05 jan '25 Maart: 03 mrt '25 - 07 mrt '25 April: 21 apr '25 - 05 mei '25 Juli: 
+        14 jul '25 - 22 aug '25</h2>
+    </p>
+</div>
+
+

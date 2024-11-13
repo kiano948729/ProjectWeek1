ef63a873193cf278a1e6fd398197fd28d2b4bd8d
kiano948729 <148862757+kiano948729@users.noreply.github.com>
2024-11-13 08:57:24 +0100

    index
    d

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php
-$current_page = basename($_SERVER['PHP_SELF']);
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,6 +7,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
     <link rel="stylesheet" href="../css/style.css">
     <script src="https://kit.fontawesome.com/5321476408.js" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/61f819b69c.js" crossorigin="anonymous"></script>
+    <title>ROC Nijmegen - Welkom</title>
 </head>
 
 <body>
@@ -17,19 +15,20 @@ $current_page = basename($_SERVER['PHP_SELF']);
         <?php require_once("components/header.php"); ?>
     </header>
     <main>
-        <div>
-            <div class="one">
-                <h1>ROC Nijmegen</h1>
+        <section class="hero-section">
+            <div class="hero-content">
+                <h1>Welkom bij ROC Nijmegen</h1>
+                <p>Ontdek een wereld vol mogelijkheden en leertrajecten!</p>
+                <a href="#discover" class="cta-button">Ontdek meer</a>
             </div>
-            <div class="img-div">
-                <img src="img/image copy 3.png">
+            <div class="hero-image">
+                <img src="img/image copy 3.png" alt="Afbeelding van ROC Nijmegen">
             </div>
-        </div>
+        </section>
     </main>
     <footer>
         <?php require_once("components/footer.php"); ?>
     </footer>
-
 </body>
 
 </html>

c879fecf077dcb61297760318f8684945f3afea1
kiano948729 <148862757+kiano948729@users.noreply.github.com>
2024-11-13 13:34:08 +0100

    as
    asdfadsf

--- components/footer.php
+++ components/footer.php
@@ -7,7 +7,6 @@
     <div class="footer-body">
         <link rel="stylesheet" href="css/style.css">
 
-<<<<<<< HEAD
 <footer class="Footer-Bottom-row">
     <div class="Footer-invisible-left-row-bottom"></div>
         <nav class="Footer-middle-row-bottom">
@@ -49,47 +48,3 @@
         </div>
 </footer>
 </body>
-=======
-        <footer class="Footer-Bottom-row">
-            <div class="Footer-invisible-left-row-bottom"></div>
-            <nav class="Footer-middle-row-bottom">
-                <div class="Footer-middle-row-texts">
-                    <a href="index.php" class="Footer-White-Text">Home</a>
-                </div>
-                <div class="Footer-middle-row-texts">
-                    <a href="resource/lessen.php" class="Footer-White-Text">Lessen</a>
-                </div>
-                <div class="Footer-middle-row-texts">
-                    <a href="reizen.php" class="Footer-White-Text">Reizen</a>
-                </div>
-                <div class="Footer-middle-row-texts">
-                    <a href="overig.php" class="Footer-White-Text">Overige</a>
-                </div>
-            </nav>
-            <div class="Footer-invisible-right-row-bottom"></div>
-            <div class="Footer-right-row-bottom">
-                <div class="Footer-icons-bottom-row">
-                    <a class="Footer-Link" href="https://www.youtube.com/@rocnijmegen/videos">
-                        <i class="fa-brands fa-youtube"></i>
-                    </a>
-                </div>
-                <div class="Footer-icons-bottom-row">
-                    <a class="Footer-Link" href="https://www.facebook.com/rocnijmegen/?locale=nl_NL">
-                        <i class="fa-brands fa-facebook-f"></i>
-                    </a>
-                </div>
-                <div class="Footer-icons-bottom-row">
-                    <a class="Footer-Link" href="https://www.instagram.com/rocnijmegen/">
-                        <i class="fa-brands fa-instagram"></i>
-                    </a>
-                </div>
-                <div class="Footer-icons-bottom-row">
-                    <a class="Footer-Link" href="https://x.com/rocnijmegen">
-                        <i class="fa-brands fa-twitter"></i>
-                    </a>
-                </div>
-            </div>
-    </div>
-    </footer>
-</body>
->>>>>>> 9173cf3d0004dfde5ca3b797ff40e45e7e17d830

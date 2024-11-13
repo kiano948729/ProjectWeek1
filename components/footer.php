aa425d30204a1c3bac36f8328cca91815103667a
Joe74012 <joewillems@upcmail.nl>
2024-11-13 11:16:27 +0100

    l

--- components/footer.php
+++ components/footer.php
@@ -1,39 +1,48 @@
-<link rel="stylesheet" href="css/style.css">
+<head>
+    <script src="https://kit.fontawesome.com/61f819b69c.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <link rel="stylesheet" href="css/style.css">
 
-<footer class="Bottom-row">
-    <div class="invisible-left-row-bottom"></div>
-        <nav class="middle-row-bottom">
-            <div class="middle-row-texts">
-                <a href="About.html" class="White-Text">About</a>
+<footer class="Footer-Bottom-row">
+    <div class="Footer-invisible-left-row-bottom"></div>
+        <nav class="Footer-middle-row-bottom">
+            <div class="Footer-middle-row-texts">
+                <a href="About.html" class="Footer-White-Text">Home</a>
             </div>
-            <div class="middle-row-texts">
-                <h3 class="White-Text">Jobs</h3>
+            <div class="Footer-middle-row-texts">
+                <a href="About.html" class="Footer-White-Text">Lessen</a>
             </div>
-            <div class="middle-row-texts">
-                <h3 class="yellow-Text">Legal</h3>
+            <div class="Footer-middle-row-texts">
+                <a href="About.html" class="Footer-White-Text">Reizen</a>
+            </div>
+            <div class="Footer-middle-row-texts">
+                <a href="About.html" class="Footer-White-Text">Overige</a>
             </div>
         </nav>
-        <div class="invisible-right-row-bottom"></div>
-        <div class="right-row-bottom">
-            <div class="icons-bottom-row">
-                <a href="https://www.youtube.com/@rocnijmegen/videos">
+        <div class="Footer-invisible-right-row-bottom"></div>
+        <div class="Footer-right-row-bottom">
+            <div class="Footer-icons-bottom-row">
+                <a class="Footer-Link" href="https://www.youtube.com/@rocnijmegen/videos">
                     <i class="fa-brands fa-square-youtube"></i>
                 </a>
             </div>
-            <div class="icons-bottom-row">
-                <a href="https://www.facebook.com/rocnijmegen/?locale=nl_NL">
+            <div class="Footer-icons-bottom-row">
+                <a class="Footer-Link" href="https://www.facebook.com/rocnijmegen/?locale=nl_NL">
                     <i class="fa-brands fa-square-facebook"></i>
                 </a>
             </div>
-            <div class="icons-bottom-row">
-                <a href="https://www.instagram.com/rocnijmegen/">
+            <div class="Footer-icons-bottom-row">
+                <a class="Footer-Link" href="https://www.instagram.com/rocnijmegen/">
                     <i class="fa-brands fa-square-instagram"></i>
                 </a>
             </div>
-            <div class="icons-bottom-row">
-                <a href="https://x.com/rocnijmegen">
+            <div class="Footer-icons-bottom-row">
+                <a class="Footer-Link" href="https://x.com/rocnijmegen">
                     <i class="fa-brands fa-square-x-twitter"></i>
                 </a>
             </div>
         </div>
 </footer>
+</body>

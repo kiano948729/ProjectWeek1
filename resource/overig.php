8656c49909991609c87b1a4c6db423e406932013
Joe74012 <joewillems@upcmail.nl>
2024-11-13 10:37:00 +0100

    w

--- resource/overig.php
+++ resource/overig.php
@@ -1,15 +1,48 @@
-<div class="ContainerBedrijven">
-    <h1 class="TextZwart">3 bedrijven</h1>
-    <p>    
-        <h2 class="TextZwart">Er zijn verschillende bedrijven die java gebruiken, zoals Portbase, Picnic en Axiom Software Solutions</h2>
-    </p>
+<head>
+    <script src="https://kit.fontawesome.com/61f819b69c.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="overig-FullContainer">
+        <div class="overig-ContainerBedrijven">
+            <div class="overig-ContainerCenter">
+                <p>
+                    <h1 class="overig-White-Text">Bedrijven die java gebruiken</h1>
+                    <div class="overig-row">
+                        <h2><a class="overig-White-Text" href="https://www.portbase.com/">Portbase</a></h2>
+                    </div>
+                    <div class="overig-row">
+                        <h2><a class="overig-White-Text" href="https://picnic.app/nl">Picnic</a></h2>
+                    </div>
+                    <div class="overig-row">
+                        <h2><a class="overig-White-Text" href="https://www.axiomsoftwaresolutions.com/">Axiom Software Solutions</a></h2>
+                    </div>
+                </p>
+            </div>
+        </div>
+    
+    <div class="overig-ContainerVakanties">
+        <div class="overig-ContainerCenter">
+            <p>
+                <h1 class="overig-White-Text">Vakanties</h1>
+                <div class="overig-row">
+                    <i class="fa-solid fa-calendar-days"></i>
+                    <h2 class="overig-White-Text">23 dec '24 - 05 jan '25</h2>
+                </div>
+                <div class="overig-row">
+                    <i class="fa-solid fa-calendar-days"></i>
+                    <h2 class="overig-White-Text">03 mrt '25 - 07 mrt '25</h2>
+                </div>
+                <div class="overig-row">
+                    <i class="fa-solid fa-calendar-days"></i>
+                    <h2 class="overig-White-Text">21 apr '25 - 05 mei '25</h2>
+                </div>
+                <div class="overig-row">
+                    <i class="fa-solid fa-calendar-days"></i>
+                    <h2 class="overig-White-Text">14 jul '25 - 22 aug '25</h2>
+                </div>
+            </p>
+        </div>
+    </div>
 </div>
-<div class="ContainerVakanties">
-    <h1 class="TextZwart">Vakanties</h1>
-    <p>    
-        <h2 class="TextZwart">De vakantie tijden zijn: December: 23 dec '24 - 05 jan '25 Maart: 03 mrt '25 - 07 mrt '25 April: 21 apr '25 - 05 mei '25 Juli: 
-        14 jul '25 - 22 aug '25</h2>
-    </p>
-</div>
-
-
+</body>

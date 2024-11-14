581b451192992b7178fd0442ce6c4c23d77d7c67
kiano948729 <148862757+kiano948729@users.noreply.github.com>
2024-11-14 12:48:15 +0100

    update footer

--- components/footer.php
+++ components/footer.php
@@ -8,28 +8,9 @@
 
 <body>
     <div class="footer-body">
-
         <footer class="Footer-Bottom-row">
             <div class="Footer-invisible-left-row-bottom">
                 <a href="../index.php"><img src="../img/gif/Ontwerp zonder titel (6).gif" alt="Logo"></a>
-        <link rel="stylesheet" href="css/style.css">
-        <!-- Start Footer-->
-<footer class="Footer-Bottom-row">
-    <div class="Footer-invisible-left-row-bottom"></div>
-    <nav class="nav-links">
-            <a href="../index.php" class="<?php echo $current_page == 'index.php' ? 'active' : ''; ?>">Home</a>
-            <a href="../lessen.php" class="<?php echo $current_page == 'lessen.php' ? 'active' : ''; ?>">Lessen</a>
-            <a href="../reizen.php"
-                class="<?php echo $current_page == 'reizen.php' ? 'active' : ''; ?>">Voorzieningen</a>
-            <a href="../resource/overig.php"
-                class="<?php echo $current_page == 'overig.php' ? 'active' : ''; ?>">Overig</a>
-        </nav>
-        <div class="Footer-invisible-right-row-bottom"></div>
-        <div class="Footer-right-row-bottom">
-            <div class="Footer-icons-bottom-row">
-                <a class="Footer-Link" href="https://www.youtube.com/@rocnijmegen/videos">
-                    <i class="fa-brands fa-youtube"></i>
-                </a>
             </div>
             <nav class="nav-links">
                 <a href="../index.php" class="<?php echo $current_page == 'index.php' ? 'active' : ''; ?>">Home</a>
@@ -61,8 +42,6 @@
                         <i class="fa-brands fa-twitter"></i>
                 </div>
             </div>
-        </div>
-        </div>
-        <!-- Einde Footer-->
-</footer>
+    </div>
+    </footer>
 </body>
